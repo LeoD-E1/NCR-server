9fb88a1d53c103ad89e3e2def811e7ef9c4b88b6
leod-e1 <leodangeli11@gmail.com>
2022-12-30 15:58:11 -0300

    [update] adding a route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { Router } from "restify-router";
 import accountRouter from "./account/account.routes";
 import transferRouter from "./transfer/transfer.routes";
 import clientRouter from "./client/client.routes";
+import { Request, Response } from "restify";
 // importing routes
 
 const router = new Router();
@@ -12,6 +13,9 @@ const listOfRoutes = new Router();
 
 router.add("/api/v1", listOfRoutes);
 
+listOfRoutes.get("/", (req: Request, res: Response) => {
+  res.json({ message: "V1 of api " });
+});
 listOfRoutes.add("/account", accountRouter);
 listOfRoutes.add("/transfer", transferRouter);
 listOfRoutes.add("/client", clientRouter);
